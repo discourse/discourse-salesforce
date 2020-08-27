4d4f411a0c3d4d1c8990867b599484acf0f4614d
Vinoth Kannan <svkn.87@gmail.com>
2020-08-28 02:03:26 +0530

    FEATURE: add new `create contact` button in topic footer.

---
+++ app/controllers/salesforce/persons_controller.rb
@@ -0,0 +1,28 @@
+class Salesforce::PersonsController < Admin::AdminController
+  before_action :find_topic
+  attr_accessor :topic
+
+  def create
+    begin
+      Salesforce::Person.create!(params.require(:type), topic.user)
+      render json: success_json
+    rescue => e
+      render json: { errors: [e.message] }, status: 422
+    end
+  end
+
+  def create_contact
+    begin
+      Salesforce::Contact.create!(topic.user)
+      render json: success_json
+    rescue => e
+      render json: { errors: [e.message] }, status: 422
+    end
+  end
+
+  def find_topic
+    params.require(:topic_id)
+    topic_id = params[:topic_id]
+    @topic = Topic.find_by(id: topic_id)
+  end
+end

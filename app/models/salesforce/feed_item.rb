1c72f25b71c2e1468ed3ec7822980ff04f7675cf
Vinoth Kannan <svkn.87@gmail.com>
2022-02-02 22:31:54 +0530

    DEV: add site setting to limit max no of feed items can be posted.

--- app/models/salesforce/feed_item.rb
+++ app/models/salesforce/feed_item.rb
@@ -7,6 +7,10 @@ module ::Salesforce
     def self.create!(post, uid)
       return if post.post_type != Post.types[:regular]
 
+      max_feed_items = SiteSetting.salesforce_max_feed_items_per_day
+      limiter = RateLimiter.new(nil, "salesforce_feed_#{uid}", max_feed_items, 1.day)
+      limiter.performed! unless max_feed_items == -1 || limiter.can_perform?
+
       body = post.raw
       body = yield(body) if block_given?
 
@@ -20,6 +24,7 @@ module ::Salesforce
       }
 
       data = Api.new.post("sobjects/FeedItem", payload)
+      limiter.performed! if max_feed_items > -1 && post.post_number > 1
 
       post.custom_fields[ID_FIELD] = data["id"]
       post.save_custom_fields

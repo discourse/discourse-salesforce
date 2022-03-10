4d401bc3d0f10c0b45b90dc1fa1c15b67f8bc049
Vinoth Kannan <vinoth.kannan@discourse.org>
2022-03-10 12:22:01 +0530

    Update db/migrate/20220126072957_create_salesforce_cases_tables.rb
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

--- db/migrate/20220126072957_create_salesforce_cases_tables.rb
+++ db/migrate/20220126072957_create_salesforce_cases_tables.rb
@@ -5,7 +5,7 @@ class CreateSalesforceCasesTables < ActiveRecord::Migration[6.0]
     create_table :salesforce_cases do |t|
       t.string :uid, null: true, index: true, unique: true
       t.integer :topic_id, null: false, unique: true
-      t.string :contact_id, null: true
+      t.string :contact_id
       t.string :number
       t.string :subject
       t.string :description

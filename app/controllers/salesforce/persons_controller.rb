a7d3739b9ecf175345ebda95fe03722766914565
Vinoth Kannan <svkn.87@gmail.com>
2022-04-22 01:31:15 +0530

    DEV: raise ArgumentError for invalid type param
    And some minor refactoring.

--- app/controllers/salesforce/persons_controller.rb
+++ app/controllers/salesforce/persons_controller.rb
@@ -1,36 +1,36 @@
 # frozen_string_literal: true
 
-class Salesforce::PersonsController < Admin::AdminController
-  before_action :find_user
-  attr_accessor :user
+module Salesforce
+  class PersonsController < ::Admin::AdminController
+    before_action :find_user
+    attr_accessor :user
 
-  def create
-    begin
-      type = params.require(:type)
-      if type == "lead"
-        Salesforce::Lead.create!(user)
-      elsif type == "contact"
-        Salesforce::Contact.create!(user)
+    def create
+      type = params.require(:type).capitalize
+      raise ArgumentError.new :type if [Lead::OBJECT_NAME, Contact::OBJECT_NAME].exclude?(type)
+
+      begin
+        "::Salesforce::#{type}".constantize.create!(user)
+        render json: success_json
+      rescue => e
+        render json: { errors: [e.message] }, status: 422
       end
-      render json: success_json
-    rescue => e
-      render json: { errors: [e.message] }, status: 422
     end
-  end
 
-  def create_contact
-    begin
-      Salesforce::Contact.create!(user)
-      render json: success_json
-    rescue => e
-      render json: { errors: [e.message] }, status: 422
+    def create_contact
+      begin
+        Contact.create!(user)
+        render json: success_json
+      rescue => e
+        render json: { errors: [e.message] }, status: 422
+      end
     end
-  end
 
-  def find_user
-    params.require(:user_id)
-    user_id = params[:user_id]
-    @user = User.find_by(id: user_id)
-    raise Discourse::InvalidParameters.new(:user_id) if user.blank?
+    def find_user
+      params.require(:user_id)
+      user_id = params[:user_id]
+      @user = User.find_by(id: user_id)
+      raise Discourse::InvalidParameters.new(:user_id) if user.blank?
+    end
   end
 end

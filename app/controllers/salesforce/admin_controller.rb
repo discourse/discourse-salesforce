c3a1e875064ffcbd5e35c91aa49e91e862cf7636
Vinoth Kannan <svkn.87@gmail.com>
2022-09-08 12:54:12 +0530

    FEATURE: add new site setting to change authorization server's url.
    Salesforce has sandbox mode where they will use different authorization server URL. Previosuly, we didn't had an option to change it.

--- app/controllers/salesforce/admin_controller.rb
+++ app/controllers/salesforce/admin_controller.rb
@@ -5,7 +5,7 @@ module Salesforce
     skip_before_action :check_xhr, :preload_json
 
     def authorize
-      redirect_to "https://login.salesforce.com/services/oauth2/authorize?client_id=#{SiteSetting.salesforce_client_id}&redirect_uri=#{Discourse.base_url}&response_type=code"
+      redirect_to "#{SiteSetting.salesforce_authorization_server_url}/services/oauth2/authorize?client_id=#{SiteSetting.salesforce_client_id}&redirect_uri=#{Discourse.base_url}&response_type=code"
     end
   end
 end

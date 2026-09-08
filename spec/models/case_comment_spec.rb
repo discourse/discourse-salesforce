6b440b90e683c6861ca137452472ace3128fd14b
Gabriel Grubba <gabriel@discourse.org>
2026-09-08 09:05:47 -0300

    FIX: Honor feed item visibility for case comments

--- spec/models/case_comment_spec.rb
+++ spec/models/case_comment_spec.rb
@@ -10,6 +10,22 @@ RSpec.describe Salesforce::CaseComment do
   fab!(:post) { Fabricate(:post, topic: topic) }
 
   describe "#create!" do
+    { "AllUsers" => true, "InternalUsers" => false }.each do |visibility, published|
+      it "sets case comment publication to #{published} for #{visibility}" do
+        SiteSetting.salesforce_feed_item_visibility = visibility
+
+        create_case_comment =
+          stub_request(:post, "#{api_path}/CaseComment").with(
+            body: hash_including("ParentId" => salesforce_case.uid, "IsPublished" => published),
+          ).to_return(status: 201, body: { id: "case_comment_123" }.to_json)
+
+        described_class.new(salesforce_case.uid, post).create!
+
+        expect(create_case_comment).to have_been_requested
+        expect(post.reload.custom_fields[described_class::ID_FIELD]).to eq("case_comment_123")
+      end
+    end
+
     it "creates a case comment object on Salesforce" do
       case_comment = ::Salesforce::CaseComment.new(salesforce_case.uid, post)
       stub_request(:post, "#{api_path}/CaseComment").with(

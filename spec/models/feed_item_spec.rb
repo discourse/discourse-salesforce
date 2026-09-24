7ff7a1a7ac1e970ed4d9928d27cb443e9662bd6e
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2026-09-24 07:34:17 -0300

    SECURITY: Prevent private messages from being exported as Salesforce FeedItems (#169)
    (cherry picked from commit cf5fcb03538ef6793c41ca17f4a16f3c7da4742e)

--- spec/models/feed_item_spec.rb
+++ spec/models/feed_item_spec.rb
@@ -25,5 +25,44 @@ RSpec.describe Salesforce::FeedItem do
 
       expect(post.custom_fields[::Salesforce::FeedItem::ID_FIELD]).to eq("feed_item_123")
     end
+
+    it "does not export private message replies or consume their quota" do
+      SiteSetting.salesforce_max_feed_items_per_day = 1
+      private_post =
+        Fabricate(
+          :post,
+          user: user,
+          topic: Fabricate(:private_message_topic, user: user),
+          post_number: 2,
+        )
+      feed_item = ::Salesforce::FeedItem.new("lead_123", private_post)
+      salesforce_request =
+        stub_request(:post, "#{api_path}/FeedItem").to_return(
+          status: 200,
+          body: { id: "feed_item_123" }.to_json,
+        )
+      RateLimiter.enable
+      limiter =
+        RateLimiter.new(
+          nil,
+          "#{::Salesforce::FeedItem::ID_FIELD}_lead_123",
+          SiteSetting.salesforce_max_feed_items_per_day,
+          1.day,
+        )
+      limiter.clear!
+
+      expect(private_post.post_type).to eq(Post.types[:regular])
+      expect(private_post.post_number).to eq(2)
+      expect(limiter.can_perform?).to eq(true)
+
+      feed_item.create!
+
+      expect(salesforce_request).not_to have_been_requested
+      expect(private_post.custom_fields[::Salesforce::FeedItem::ID_FIELD]).to be_nil
+      expect(limiter.remaining).to eq(1)
+    ensure
+      limiter&.clear!
+      RateLimiter.disable
+    end
   end
 end
